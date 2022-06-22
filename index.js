c52e3bd7286b568b7cef9ff9f2733c4ae4cf075b
Volodymyr Kocherhan <volodymyrkocherhan@gmail.com>
2022-06-22 14:55:07 +0300

    cards-list

---
+++ index.js
@@ -0,0 +1,82 @@
+import Pagination from './pagination.js';
+import CardsList from './cards-list.js';
+
+const BACKEND_URL = 'https://online-store.bootcamp.place/api/';
+export default class OnlineStorePage {
+  constructor() {
+    this.pageSize = 9;
+    this.products = [];
+
+    this.url = new URL('products', BACKEND_URL);
+    this.url.searchParams.set('_limit', this.pageSize);
+
+    this.components = {};
+    this.initComponents();
+    this.render();
+    this.renderСomponents();
+    this.initEventlisteners();
+    this.update(1);
+  }
+  async loadData(pageNumber) {
+    this.url.searchParams.set('_page', pageNumber);
+
+    const responce = await fetch(this.url);
+    const products = await responce.json();
+    return products;
+  }
+
+  getTemplate() {
+    return `
+      <div>
+        <div data-element="cardsList">
+          <!-- Card component -->
+        </div>
+        <div data-element="pagination">
+          <!-- Pagination component -->
+        </div>
+      </div>
+    `;
+  }
+  initComponents() {
+    const totalElements = 100;
+    const totalPages = Math.ceil(totalElements / this.pageSize);
+
+    const cardsList = new CardsList(this.products);
+
+    const pagination = new Pagination({
+      activePageIndex: 0,
+      totalPages: totalPages,
+    });
+    this.components.cardsList = cardsList;
+    this.components.pagination = pagination;
+  }
+
+  renderСomponents() {
+    const cardsConteiner = this.element.querySelector(
+      '[data-element="cardsList"]',
+    );
+    const paginationConteiner = this.element.querySelector(
+      '[data-element="pagination"]',
+    );
+    cardsConteiner.append(this.components.cardsList.element);
+    paginationConteiner.append(this.components.pagination.element);
+  }
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper.firstElementChild;
+  }
+  initEventlisteners() {
+    this.components.pagination.element.addEventListener(
+      'page-changed',
+      event => {
+        const pagIndex = event.detail;
+        this.update(pagIndex + 1);
+      },
+    );
+  }
+  async update(pageNumber) {
+    const productsList = await this.loadData(pageNumber);
+    this.components.cardsList.update(productsList);
+  }
+}
---
+++ pagination.js
@@ -0,0 +1,102 @@
+export default class Pagination {
+  constructor({ actinePageIndex = 0, totalPages = 0 } = {}) {
+    this.actinePageIndex = actinePageIndex;
+    this.totalPages = totalPages;
+    this.render();
+    this.addEventListeners();
+  }
+  getTemplate() {
+    return `
+    <nav class="card-pagination font-montserrat-400">
+      <a href="#" class="panigation-arrow-left" data-element="nav-prev" ><i class="bi bi-chevron-left"></i></a>
+        <div>${this.getPages()}</div>
+      <a href="#" class="panigation-arrow-right" data-element="nav-next" ><i class="bi bi-chevron-right"></i></a>
+    </nav>
+    `;
+  }
+  getPages() {
+    return `
+            <ul class="pagination-block" data-element="pagination">
+
+           ${new Array(this.totalPages)
+             .fill(1)
+             .map((item, index) => {
+               return this.getPageTemplate(index);
+             })
+             .join('')}
+        </ul>
+    `;
+  }
+  getPageTemplate(pageIndex = 0) {
+    const isActive = pageIndex === this.actinePageIndex ? 'active' : '';
+    return `<li class="pagination-item ">
+    <a href="#"
+    class="page-link ${isActive}"
+    data-page-index="${pageIndex}"> ${pageIndex + 1} </a></li>`;
+  }
+  setPage(pageIndex = 0) {
+    if (pageIndex === this.actinePageIndex) return;
+    if (pageIndex > this.totalPages - 1 || pageIndex < 0) return;
+
+    this.dispatchSomeEvent(pageIndex);
+
+    const activePage = this.element.querySelector('.card-pagination .active');
+    if (activePage) {
+      activePage.classList.remove('active');
+    }
+    const page = this.element.querySelector(`[data-page-index="${pageIndex}"]`);
+    if (page) {
+      page.classList.add('active');
+    }
+
+    this.actinePageIndex = pageIndex;
+  }
+  nextPage() {
+    const nextPage = this.actinePageIndex + 1;
+    this.setPage(nextPage);
+  }
+  prevPage() {
+    const prevPage = this.actinePageIndex - 1;
+    this.setPage(prevPage);
+  }
+
+  render() {
+    const wraper = document.createElement('div');
+    wraper.innerHTML = this.getTemplate();
+    this.element = wraper;
+  }
+
+  addEventListeners() {
+    const prevPageBtn = this.element.querySelector('[data-element="nav-prev"]');
+    const nextPageBtn = this.element.querySelector('[data-element="nav-next"]');
+    const pageList = this.element.querySelector('[data-element="pagination"]');
+
+    prevPageBtn.addEventListener('click', event => {
+      this.prevPage();
+    });
+
+    nextPageBtn.addEventListener('click', event => {
+      this.nextPage();
+    });
+
+    pageList.addEventListener('click', event => {
+      const pageItem = event.target.closest('.page-link');
+
+      if (!pageItem) return;
+
+      const pageIndex = pageItem.dataset.pageIndex;
+
+      // this.setPage(+pageIndex);  //* переводимо стрічку в число
+      //або
+      this.setPage(parseInt(pageIndex, 10)); //* переводимо стрічку в число
+    });
+  }
+
+  dispatchSomeEvent(pageIndex) {
+    const customEvent = new CustomEvent('page-changed', {
+      detail: pageIndex,
+    });
+
+    this.element.dispatchEvent(customEvent);
+  }
+}
